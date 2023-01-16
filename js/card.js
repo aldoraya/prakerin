5408580871a0e65122d7a8ade295de9669934a6d
aldoraya <aldorayaalhakim@gmail.com>
2023-01-16 12:11:36 +0700

    udate

--- js/card.js
+++ js/card.js
@@ -1,41 +1,147 @@
 $('#card1').click(function(){
-    swal({
-        text: 'Nama: Alvio Adji Januar \n \n Nis: 202110291 \n \n Kelas: XII Rekayasa Perangkat Lunak 1 \n \n Agama: Islam \n \n Tempat, Tanggal lahir: Bogor, 15 Januari 2006 \n \n Alamat Tempat Tinggal: Jl. Lb. Bambu No. 15, Bojong Kulur, Kec. Gn. Putri, Kabupaten Bogor, Jawa Barat 16969 \n \n Alamat Prakerin: PT. BANGUN KREATIF ABADI \n \n Mulai Prakerin: 17 Januari 2022 \n \n Selesai Prakerin: 17 April 2022 ',
+    swal.fire({
+        title: 'Data Pribadi',
+        html: "<br>" +
+        "<p class='text-left'>Nama : Alvio Adji Januar </p>" +
+         "<br>" +
+        "<p class='text-left'>Nis : 202110291</p>" +
+        "<br>" +
+        "<p class='text-left'>Kelas : XII Rekayasa Perangkat Lunak 1</p>" +
+        "<br>" +
+        "<p class='text-left'>Agama : Islam</p>" +
+        "<br>" +
+        "<p class='text-left'>Tempat, Tanggal lahir : Bogor, 15 Januari 2006</p>" +
+        "<br>" +
+        "<p class='text-left'>Alamat Tempat Tinggal : Jl. Lb. Bambu No. 15, Bojong Kulur, Kec. Gn. Putri, Kabupaten Bogor, Jawa Barat 16969 </p>" +
+        "<br>" +
+        "<p class='text-left'>Alamat Prakerin : PT. BANGUN KREATIF ABADI</p>" +
+        "<br>" +
+        "<p class='text-left'>Mulai Prakerin : 17 Januari 2022</p>" +
+        "<br>" +
+        "<p class='text-left'>Selesai Prakerin : 17 April 2022</p>",
+        button: 'oke',
     });
 });
 
 $('#card2').click(function(){
-    swal({
-        text: 'Nama: Aldo Raya Al Hakim \n \n Nis: 202110289 \n \n Kelas: XII Rekayasa Perangkat Lunak 1 \n \n Agama: Islam \n \n Tempat, Tanggal lahir: Bekasi, 10 Januari 2006 \n \n Alamat Tempat Tinggal: Jl. H. Abd. Karim II, RT.005/RW.009, Jatikramat, kec. Jatiasih, Kota Bekasi  \n \n Alamat Prakerin: PT. BANGUN KREATIF ABADI \n \n Mulai Prakerin: 17 Januari 2022 \n \n Selesai Prakerin: 17 April 2022 ',
+    swal.fire({
+        title: 'Data Pribadi',
+        html: "<br>" +
+        "<p class='text-left'>Nama : Aldo Raya Al Hakim</p>" +
+         "<br>" +
+        "<p class='text-left'>Nis : 202110289</p>" +
+        "<br>" +
+        "<p class='text-left'>Kelas : XII Rekayasa Perangkat Lunak 1</p>" +
+        "<br>" +
+        "<p class='text-left'>Agama : Islam</p>" +
+        "<br>" +
+        "<p class='text-left'>Tempat, Tanggal lahir : Bekasi, 10 Januari 2006</p>" +
+        "<br>" +
+        "<p class='text-left'>Alamat Tempat Tinggal : Jl. H. Abd. Karim II, RT.005/RW.009, Jatikramat, kec. Jatiasih, Kota Bekasi</p>" +
+        "<br>" +
+        "<p class='text-left'>Alamat Prakerin : PT. BANGUN KREATIF ABADI</p>" +
+        "<br>" +
+        "<p class='text-left'>Mulai Prakerin : 17 Januari 2022</p>" +
+        "<br>" +
+        "<p class='text-left'>Selesai Prakerin : 17 April 2022</p>",
+        button: 'oke',
     })
 })
 
 $('#card3').click(function(){
-    swal({
-        text: 'Nama: Muhammad Ezar Putrandanu \n \n Nis: 202110345 \n \n Kelas: XII Rekayasa Perangkat Lunak 2 \n \n Agama: Islam \n \n Tempat, Tanggal lahir: Bekasi, 06 Februari 2005 \n \n Alamat Tempat Tinggal: Cluster Kiana Cikunir Blok A10 \n \n Alamat Prakerin: PT. BANGUN KREATIF ABADI \n \n Mulai Prakerin: 17 Januari 2022 \n \n Selesai Prakerin: 17 April 2022 ',
+    swal.fire({
+        title: 'Data Pribadi',
+        html: "<br>" +
+        "<p class='text-left'>Nama : Muhammad Ezar Putrandanu</p>" +
+         "<br>" +
+        "<p class='text-left'>Nis : 202110345</p>" +
+        "<br>" +
+        "<p class='text-left'>Kelas : XII Rekayasa Perangkat Lunak 2</p>" +
+        "<br>" +
+        "<p class='text-left'>Agama : Islam</p>" +
+        "<br>" +
+        "<p class='text-left'>Tempat, Tanggal lahir : Bekasi, 06 Februari 2005</p>" +
+        "<br>" +
+        "<p class='text-left'>Alamat Tempat Tinggal : Cluster Kiana Cikunir Blok A10</p>" +
+        "<br>" +
+        "<p class='text-left'>Alamat Prakerin : PT. BANGUN KREATIF ABADI</p>" +
+        "<br>" +
+        "<p class='text-left'>Mulai Prakerin : 17 Januari 2022</p>" +
+        "<br>" +
+        "<p class='text-left'>Selesai Prakerin : 17 April 2022</p>",
+        button: 'oke',
     });
 });
 
 $('#card4').click(function(){
-    swal({
-        text: 'Nama: Lintang Mulia Kusumawardani \n \n Nis: 202110377 \n \n Kelas: XII Rekayasa Perangkat Lunak 3 \n \n Agama: Islam \n \n Tempat, Tanggal lahir: Bekasi, 27 Juni 2004 \n \n Alamat Tempat Tinggal: Perum. Taman Rahayu Regenerasi 1 Blok A 11 No.251 RT.09/RW.08 \n \n Alamat Prakerin: PT. BANGUN KREATIF ABADI \n \n Mulai Prakerin: 17 Januari 2022 \n \n Selesai Prakerin: 17 April 2022 ',
+    swal.fire({
+        title: 'Data Pribadi',
+        html: "<br>" +
+        "<p class='text-left'>Nama : Lintang Mulia Kusumawardani</p>" +
+         "<br>" +
+        "<p class='text-left'>Nis : 202110377</p>" +
+        "<br>" +
+        "<p class='text-left'>Kelas : XII Rekayasa Perangkat Lunak 3</p>" +
+        "<br>" +
+        "<p class='text-left'>Agama : Islam</p>" +
+        "<br>" +
+        "<p class='text-left'>Tempat, Tanggal lahir : Bekasi, 27 Juni 2004</p>" +
+        "<br>" +
+        "<p class='text-left'>Alamat Tempat Tinggal : Perum. Taman Rahayu Regenerasi 1 Blok A 11 No.251 RT.09/RW.08</p>" +
+        "<br>" +
+        "<p class='text-left'>Alamat Prakerin : PT. BANGUN KREATIF ABADI</p>" +
+        "<br>" +
+        "<p class='text-left'>Mulai Prakerin : 17 Januari 2022</p>" +
+        "<br>" +
+        "<p class='text-left'>Selesai Prakerin : 17 April 2022</p>",
+        button: 'oke',
     });
 });
 
 $('#card5').click(function(){
-    swal({
-        text: 'Nama: Irma Juanita \n \n Nis: 202110339 \n \n Kelas: XII Rekayasa Perangkat Lunak 2 \n \n Agama: Islam \n \n Tempat, Tanggal lahir: Jakarta, 02 Juni 2004 \n \n Alamat Tempat Tinggal: Perum.Taman Rahayu Regency 1 Jln. Flamboyan 3 Blok B13 No.283 RT.10/RW.08 \n \n Alamat Prakerin: PT. BANGUN KREATIF ABADI \n \n Mulai Prakerin: 17 Januari 2022 \n \n Selesai Prakerin: 17 April 2022 ',
+    swal.fire({
+        title: 'Data Pribadi',
+         html: "<br>" +
+        "<p class='text-left'>Nama : Irma Juanita</p>" +
+         "<br>" +
+        "<p class='text-left'>Nis : 202110339</p>" +
+        "<br>" +
+        "<p class='text-left'>Kelas : XII Rekayasa Perangkat Lunak 2</p>" +
+        "<br>" +
+        "<p class='text-left'>Agama : Islam</p>" +
+        "<br>" +
+        "<p class='text-left'>Tempat, Tanggal lahir : Jakarta, 02 Juni 2004</p>" +
+        "<br>" +
+        "<p class='text-left'>Alamat Tempat Tinggal : Perum.Taman Rahayu Regency 1 Jln. Flamboyan 3 Blok B13 No.283 RT.10/RW.08</p>" +
+        "<br>" +
+        "<p class='text-left'>Alamat Prakerin : PT. BANGUN KREATIF ABADI</p>" +
+        "<br>" +
+        "<p class='text-left'>Mulai Prakerin : 17 Januari 2022</p>" +
+        "<br>" +
+        "<p class='text-left'>Selesai Prakerin : 17 April 2022</p>",
+        button: 'oke',
     });
 });
 
 $('#card-visi').click(function(){
-    swal({
-        text: 'VISI :\n 1. Menjadi penyedia solusi dan layanan TI terkemuka di industri. \n \n 2. Membangun kemitraan jangka panjang dengan pelanggan untuk mendukung perjalanan transformasi digital mereka. \n \n 3. Berkontribusi pada pengembangan industri TI di negara atau wilayah. \n \n MISI: \n  1. Memberikan solusi IT yang inovatif untuk meningkatkan efisiensi dan efektifitas bisnis. \n \n 2. Memberikan layanan TI yang berkualitas tinggi dan andal untuk memenuhi kebutuhan pelanggan. \n \n 3. Berinvestasi dalam penelitian dan pengembangan untuk tetap berada di garis depan kemajuan teknologi.'
+    swal.fire({
+        html: "<div class='text-center font-bold text-xl'>Visi </div>" +
+        "<br>" +
+        "<p class='text-left block'>1. Menjadi penyedia solusi dan layanan TI terkemuka di industri.</p>" +
+        "<p class='text-left block'>2. Membangun kemitraan jangka panjang dengan pelanggan untuk mendukung perjalanan transformasi digital mereka</p>" +
+        "<p class='text-left block'>3. Berkontribusi pada pengembangan industri TI di negara atau wilayah.</p>" +
+        "<br>" +
+        "<div class='text-center font-bold text-xl'>Misi </div>" +
+        "<br>" +
+        "<p class='text-left block'>1. Memberikan solusi IT yang inovatif untuk meningkatkan efisiensi dan efektifitas bisnis.</p>" +
+        "<p class='text-left block'>2. Memberikan layanan TI yang berkualitas tinggi dan andal untuk memenuhi kebutuhan pelanggan.</p>" +
+        "<p class='text-left block'>3. Berinvestasi dalam penelitian dan pengembangan untuk tetap berada di garis depan kemajuan teknologi.</p>",
+        button: 'oke',
     })
 })
 
 $('#card-visi2').click(function(){
-    swal({
+    swal.fire({
         text: 'VISI :\n 1. Menjadi penyedia solusi dan layanan TI terkemuka di industri. \n \n 2. Membangun kemitraan jangka panjang dengan pelanggan untuk mendukung perjalanan transformasi digital mereka. \n \n 3. Berkontribusi pada pengembangan industri TI di negara atau wilayah. \n \n MISI: \n  1. Memberikan solusi IT yang inovatif untuk meningkatkan efisiensi dan efektifitas bisnis. \n \n 2. Memberikan layanan TI yang berkualitas tinggi dan andal untuk memenuhi kebutuhan pelanggan. \n \n 3. Berinvestasi dalam penelitian dan pengembangan untuk tetap berada di garis depan kemajuan teknologi.'
     })
 })
